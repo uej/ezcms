7d9163820d5d156ebeb2523e6a48a71a8f55dabf
uej <534883041@qq.com>
2018-02-11 23:06:59 +0800

    cc
    cc

--- ez/ez.php
+++ ez/ez.php
@@ -1,40 +1,5 @@
 <?php
 /**
- * 系统函数
- * 
- * @author lxj
- */
-
-/**
- * 框架自动加载函数
- * 
- * @param string $classname 类名
- */
-function ezAutoload($classname)
-{
-    if (false !== strpos($classname, '\\')) {
-            
-        /* 定位路径 */
-        $filename = __DIR__ . '/../' . str_replace('\\', '/', $classname . '.php');
-
-        /* 引入文件 */
-        if (is_file($filename)) {
-            include $filename;
-        }
-    } else {
-        $filename = __DIR__ . '/core/' . $classname . '.php';
-        if (is_file($filename)) {
-            include $filename;
-        }
-    }
-    
-    return;
-}
-
-/* 注册自动加载函数 */
-spl_autoload_register('ezAutoload', TRUE, TRUE);
-
-/**
  * 获取、设置配置函数
  * 
  * @param mixed $name 配置的名称或者 key=>value 对
